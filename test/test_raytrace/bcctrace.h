6e0ba669deb740ae308fb900bef8151ac93b1284
bacchus <zukamimiridoari@gmail.com>
2016-01-12 20:44:13 +0200

    add test raytrace

---
+++ test/test_raytrace/bcctrace.h
@@ -0,0 +1,164 @@
+#pragma once
+
+#include <vector>
+
+#include "math/mdefs.h"
+#include "math/vec2.h"
+#include "math/vec3.h"
+#include "math/vec4.h"
+#include "math/mat2.h"
+#include "math/mat3.h"
+
+#include "image/image.h"
+
+#include "utils/timer.h"
+
+namespace bacchus {
+
+//----------------------------------------------------------------------
+struct Mtl {
+    vec3f col;
+};
+
+struct Prim {
+    Mtl mtl;
+    virtual float sd(const vec3f& pos) = 0;
+};
+
+struct Plane: public Prim {
+    vec3f normal;
+    virtual float sd(const vec3f &pos) override {
+        return pos.y;
+    }
+};
+
+struct Sphere: public Prim {
+    vec3f center;
+    float radius;
+    virtual float sd(const vec3f &pos) override {
+        return length(pos - center) - radius;
+    }
+};
+
+std::vector<Prim*> prims;
+
+void init_scene() {
+    Plane* p1 = new Plane;
+    p1->mtl.col = vec3f(0.8f,0.5f,0.2f);
+
+    Sphere* p2 = new Sphere;
+    p2->center = vec3f(0.0f,0.25f, 0.0f);
+    p2->radius = 0.25f;
+    p2->mtl.col = vec3f(0.2,0.7,0.2);
+
+    prims.push_back(p1);
+    prims.push_back(p2);
+}
+
+//----------------------------------------------------------------------
+
+mat3f setCamera(const vec3f& ro, const vec3f& ta, float rol) {
+    vec3f zax = normalize(ta-ro);
+    vec3f up = vec3f(std::sin(rol), std::cos(rol), 0.f);
+    vec3f xax = normalize(cross(zax,up));
+    vec3f yax = normalize(cross(xax,zax));
+    return mat3f(xax, yax, zax);
+}
+
+//----------------------------------------------------------------------
+
+Prim* find_nearest(const vec3f& pos, float& d) {
+    Prim* res = NULL;
+    d = BCC_M_FLOAT_MAX;
+    for (Prim* p: prims) {
+        float t = p->sd(pos);
+        if (t < d) {
+            d = t;
+            res = p;
+        }
+    }
+    return res;
+}
+
+
+//----------------------------------------------------------------------
+
+vec3f calcNormal(const vec3f& pos) {
+    float eps = 0.001f;
+    vec3f xyy(eps,0.f,0.f);
+    vec3f yxy(0.f,eps,0.f);
+    vec3f yyx(0.f,0.f,eps);
+
+    vec3f t[2];
+    find_nearest(pos+xyy, t[0].x);
+    find_nearest(pos-xyy, t[1].x);
+
+    find_nearest(pos+yxy, t[0].y);
+    find_nearest(pos-yxy, t[1].y);
+
+    find_nearest(pos+yyx, t[0].z);
+    find_nearest(pos-yyx, t[1].z);
+
+    return normalize(t[0] - t[1]);
+}
+
+//----------------------------------------------------------------------
+
+Prim* castRay(const vec3f& rori, const vec3f& rdir, float& dist) {
+    const int maxsteps = 50;
+    const float tmin = 1.f;
+    const float tmax = 20.f;
+    const float precis = 0.002f;
+    dist = tmin;
+    float t;
+    Prim* m = NULL;
+    for (int i = 0; i < maxsteps; ++i) {
+        Prim* resm = find_nearest(rori+rdir*dist, t);
+        if (t < precis || dist > tmax) break;
+        dist += t;
+        m = resm;
+    }
+    if(dist > tmax)
+        return NULL;
+    return m;
+}
+
+vec3f render(const vec3f& eye, const vec3f& dir) {
+    vec3f bgcolor = vec3f(0.8, 0.9, 1.0);
+    vec3f ligdir = vec3f(-0.6f, 0.7f, -0.5f);
+    vec3f col = bgcolor;
+    float t;
+    Prim* p = castRay(eye,dir, t);
+    if(p) {
+        col = p->mtl.col;
+        vec3f pos = eye + t*dir;
+        vec3f nor = calcNormal(pos);
+        vec3f lig = normalize(ligdir);
+        float dif = clamp(dot(nor,lig), 0.f, 1.f);
+        vec3f brdf = vec3f(0.0);
+        brdf += dif*vec3f(1.0f,0.9f,0.6f);
+        col = col*brdf;
+        col = mix(col, bgcolor, 1.0f-exp(-0.0005f*t*t));// fog
+    }
+    return clamp(col,0.f,1.f);
+}
+
+void process(Image& img) {
+    ScopedTimer timer("mytrace");
+    init_scene();
+    vec3f eye = vec3f(1.0f, 1.0f, 3.5f);
+    vec3f tar = vec3f(0.0f, 0.0f, 0.0f);
+    mat3f view = setCamera(eye, tar, 0.0);
+
+    vec2i fcord = 0.5f*img.resolution();
+    for (fcord.y = 0; fcord.y < img.resolution().y; ++fcord.y) {
+        for (fcord.x = 0; fcord.x < img.resolution().x; ++fcord.x) {
+            vec2f q = vec2f(fcord)/vec2f(img.resolution());
+            vec2f p = 2.f*q - vec2f(1.f);
+            vec3f raydir = view * normalize(vec3f(p,2.f));
+            img.pix(fcord) = rgb2i(255.f*render(eye, raydir));
+        }
+    }
+}
+
+} // namespace bcc
---
+++ test/test_raytrace/main.cpp
@@ -0,0 +1,142 @@
+#include <vector>
+#include <limits>
+#include <map>
+
+#include <QApplication>
+#include <QGLWidget>
+#include <QKeyEvent>
+
+#include "image/png_rw.h"
+#include "filters/filter_simple.h"
+#include "glwrap/program.h"
+
+#include "bcctrace.h"
+
+const float g_start_param = 1.0f;
+const float g_delta_param = 0.1f;
+const float g_min_param = 0.0f;
+const float g_max_param = 1.0f;
+
+using namespace bacchus;
+
+class GLTest: public QGLWidget {
+public:
+    GLTest(QWidget* parent = 0)
+        : QGLWidget(parent)
+    {
+        ALOGD("Hello kittie!");
+
+        //bacchus::read_png_mirrored("test.png", m_img);
+        m_img = Image(200,200);
+        process(m_img);
+//        Engine e;
+//        e.prims.push_back(new Plane(vec3f(0.7f,0.7f,0.7f)));
+//        e.prims.push_back(new Sphere(vec3f(0.7f,0.5f,0.2f), vec3f(0,0.25,0), 0.25f));
+//        e.mainImage(m_img);
+
+        /// setup qt window
+        setWindowTitle("Hello kittie");
+        setAttribute(Qt::WA_QuitOnClose);
+        startTimer(10);
+        m_width = m_img.width;
+        m_height = m_img.height;
+        resize(m_width, m_height);
+
+        /// opengl specific
+        m_tex = new bacchus::Texture(m_img.width, m_img.height, m_img.dat8);
+        m_filter = new bacchus::FilterSimple();
+        m_filter->resize(m_width, m_height);
+        m_filter->setInput(m_tex);
+        m_filter->setOutput(NULL);
+        m_data = g_start_param;
+    }
+
+    ~GLTest() {
+        delete m_filter;
+    }
+
+    static int main(int argc, char* argv[]) {
+        srand(time(NULL));
+        QApplication app(argc, argv);
+        GLTest widget;
+        widget.show();
+        return app.exec();
+    }
+
+    typedef void (GLTest::*on_key)(int);
+    std::map<int, on_key> key_press = {
+        {Qt::Key_Escape, &GLTest::exit_program},
+        {Qt::Key_Left, &GLTest::less},
+        {Qt::Key_Right, &GLTest::more}
+    };
+
+    void exit_program(int) { close(); }
+
+    void less(int) {
+        m_data = std::max(m_data - g_delta_param, g_min_param);
+        //m_filter->setChanged();
+    }
+
+    void more(int) {
+        m_data = std::min(m_data + g_delta_param, g_max_param);
+        //m_filter->setChanged();
+    }
+
+protected:
+    virtual void keyPressEvent(QKeyEvent* event) {
+        int key = event->key();
+        if (key_press.count(key)) {
+            (this->*key_press[key])(key);
+        }
+    }
+
+    virtual void timerEvent(QTimerEvent* /*event*/) {
+        updateGL();
+    }
+
+    virtual void updateGL() {
+        QGLWidget::updateGL();
+        paintGL();
+    }
+
+    virtual void initializeGL() {
+        // gl settings
+        CHECK_GL_ERROR(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+        CHECK_GL_ERROR(glEnable(GL_BLEND));
+        CHECK_GL_ERROR(glDisable(GL_DEPTH_TEST));
+        // clearing
+        CHECK_GL_ERROR(glClearColor(0.7, 0.5, 0.2, 1.0));
+        CHECK_GL_ERROR(glClear(GL_COLOR_BUFFER_BIT));
+        // init bacchuss
+        m_tex->createGl();
+        m_tex->pixels();
+        m_filter->init();
+    }
+
+    virtual void resizeGL(int w, int h) {
+        m_filter->resize(w, h);
+    }
+
+    virtual void paintGL() {
+        m_filter->render();
+        draw_text("Hello kittie!");
+    }
+
+    void draw_text(const std::string& s) {
+        QString text = QString(s.c_str());//.arg(mValue);
+        int length = QFontMetrics(font()).width(text);
+        glColor3f(1, 1, 1);
+        renderText(width() - length - 1, font().pointSize() + 1, text);
+    }
+
+private:
+    bacchus::Image m_img;
+    int m_width, m_height;
+    bacchus::Texture* m_tex;
+    bacchus::FilterSimple* m_filter;
+    float m_data;
+};
+
+int main(int argc, char* argv[]) {
+    return GLTest::main(argc, argv);
+}
